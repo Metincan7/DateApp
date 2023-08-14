f7ae6e357a1efea1d48bfffbd34ee98bed39f0ee
DBoss <mtncn_clk17@hotmail.com>
2023-08-14 05:09:32 +0300

    Routing AuthControl and Security

---
+++ client/src/app/_guards/auth.guard.ts
@@ -0,0 +1,20 @@
+import { Inject, Injectable } from '@angular/core';
+import { CanActivateFn } from '@angular/router';
+import { AccountService } from '../_services/account.service';
+import { ToastrService } from 'ngx-toastr';
+import { map } from 'rxjs';
+
+export const authGuard: CanActivateFn = (route, state) => {
+  const accountService=Inject(AccountService);
+  const toastr=Inject(ToastrService)
+
+  return accountService.currentUser$.pipe(
+    map(user =>{
+      if (user) return true;
+      else{
+        toastr.error('you shall not pass!');
+        return false;
+      }
+    })
+  )
+};
--- client/src/app/register/register.component.ts
+++ client/src/app/register/register.component.ts
@@ -1,6 +1,7 @@
 
 import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
 import { AccountService } from '../_services/account.service';
+import { ToastrService } from 'ngx-toastr';
 
 @Component({
   selector: 'app-register',
@@ -12,7 +13,7 @@ export class RegisterComponent implements OnInit {
 model :any={};
 
 
-constructor(private accountService:AccountService) {}
+constructor(private accountService:AccountService,private toastr:ToastrService) {}
 
 ngOnInit():void{
 }
@@ -23,7 +24,7 @@ this.accountService.register(this.model).subscribe({
   
     this.cancel()
   },
-  error:error =>console.log(error)
+  error:error =>this.toastr.error(error.error)
 })
 }
 cancel(){
